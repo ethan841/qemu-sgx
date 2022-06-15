227355f4b4bc14a52006dc3990b1316dc0cbc714
ethan841 <ethan841@gmail.com>
2022-06-15 19:17:58 +0900

    PCR server update

--- hw/tpm/tpm_tis.c
+++ hw/tpm/tpm_tis.c
@@ -222,7 +222,8 @@ static uint64_t pcr_req(void)
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  
     serv_addr.sin_family = AF_INET;  
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  
+    //serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_addr.s_addr = inet_addr("172.25.244.104");  
     serv_addr.sin_port = htons(5566); 
     connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
